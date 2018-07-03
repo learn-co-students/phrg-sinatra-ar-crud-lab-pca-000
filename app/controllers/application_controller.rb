dec6cdea75734da5eebb39671bfe6f48515c836c
Nick Wohnhas <nicholas.wohnhas@powerhrg.com>
2018-07-02 22:44:18 -0400

    sinatra CRUD

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 
 require_relative '../../config/environment'
-
+require 'pry'
 class ApplicationController < Sinatra::Base
 
   configure do
@@ -11,4 +11,54 @@ class ApplicationController < Sinatra::Base
   get '/' do
 
   end
+
+  get '/posts/new' do
+    erb :new
+  end
+
+  post '/posts' do
+
+    @post = Post.create(params)
+
+    erb :index
+  end
+
+  get '/posts' do
+    @posts = Post.all
+
+    erb :index
+  end
+
+  get '/posts/:id' do
+
+    @post = Post.find_by id: params[:id]
+
+    erb :show
+  end
+
+  get '/posts/:id/edit' do
+
+    @post = Post.find_by id: params[:id]
+
+
+    erb :edit
+  end
+
+  patch '/posts/:id' do
+
+    @post = Post.find_by(id: params[:id])
+    @post.update(name: params[:name])
+    @post.update(content: params[:content])
+    erb :edit
+
+  end
+
+  delete '/posts/:id/delete' do
+    # binding.pry
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
+
+    erb :delete
+  end
+
 end
