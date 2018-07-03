a90f6eb50d2dd4f27533800fb62f777b09d1c0ad
Daniel-Mack <daniel.mack@powerhrg.com>
2018-07-03 07:56:24 -0400

    finished lab

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,3 @@
-
 require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
@@ -8,7 +7,44 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  get '/posts/new' do
+    erb :new
+  end
 
+  get "/posts" do
+    @posts = Post.all
+    erb :index
+  end
+
+  post "/posts" do
+    Post.create(params)
+    erb :index
+  end
+
+  post "/show" do
+    erb :show
+  end
+
+  get "/posts/:id" do
+    @post = Post.find(params[:id])
+    erb :show
+  end
+
+  get "/posts/:id/edit" do
+    @post = Post.find(params[:id])
+    erb :edit
+  end
+
+    patch "/posts/:id" do
+    @post = Post.find(params[:id])
+    @post.update(name: params[:name], content: params[:content])
+    @post.save
+    redirect "/posts/#{params[:id]}"
+  end
+
+    delete "/posts/:id/delete" do
+    @post = Post.find(params[:id])
+    @post.destroy
+    erb :show
   end
 end
