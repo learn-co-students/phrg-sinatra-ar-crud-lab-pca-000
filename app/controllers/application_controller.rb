1558bc6216d0395644d85aeda26f72c3d4a8f4bf
KFad <kfadigan@yahoo.com>
2019-05-06 11:06:04 -0400

    Combine ActiveRecord and CRUD

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,45 @@ class ApplicationController < Sinatra::Base
   get '/' do
 
   end
+
+  get '/posts/new' do
+    erb :new
+  end
+
+  post '/posts' do
+    @post = Post.create(params)
+
+    redirect '/posts'
+  end
+
+  get '/posts' do
+    @posts = Post.all
+
+    erb :index
+  end
+
+  get '/posts/:id' do
+    @post = Post.find_by(id: params[:id])
+
+    erb :show
+  end
+
+  get '/posts/:id/edit' do
+    @post = Post.find_by(id: params[:id])
+
+    erb :edit
+  end
+
+  patch '/posts/:id' do
+    @post = Post.update(params[:id], name: params[:name], content: params[:content])
+
+    erb :edit
+  end
+
+  delete '/posts/:id/delete' do
+    Post.delete(params[:id])
+
+    erb :delete
+  end
+
 end
